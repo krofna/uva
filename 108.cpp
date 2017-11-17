aef4a82dbaf14c1dc10d493a113823d5ac2c9f73
Mislav Blažević <mislav.blazevic5@gmail.com>
2017-11-17 21:44:13 +0100

    Improve Maximum Sum

--- 108.cpp
+++ 108.cpp
@@ -3,13 +3,25 @@ using namespace std;
 
 int n, A[100][100];
 
-int f(int i, int j, int y, int x)
+inline int f(int i, int j, int y, int x)
 {
     int r = A[y][x];
     if (j > 0) r -= A[y][j - 1];
     if (i > 0) r -= A[i - 1][x];
     if (i > 0 && j > 0) r += A[i - 1][j - 1];
     return r;
+}
+
+inline int g(int i, int j)
+{
+    int ans = -127 * 100 * 100, sum = 0;
+    for (int k = 0; k < n; ++k)
+    {
+        sum += f(k, i, k, j);
+        ans = max(ans, sum);
+        sum = max(sum, 0);
+    }
+    return ans;
 }
 
 int main()
@@ -27,9 +39,7 @@ int main()
     }
     int ans = -127 * 100 * 100;
     for (int i = 0; i < n; ++i)
-        for (int j = 0; j < n; ++j)
-            for (int y = i; y < n; ++y)
-                for (int x = j; x < n; ++x)
-                    ans = max(ans, f(i, j, y, x));
+        for (int j = i; j < n; ++j)
+            ans = max(ans, g(i, j));
     cout << ans << '\n';
 }
